15b224c7e752c32b86cd1a05b447a3d45ec8b9a9
Brandon-Lee Dodds <19696909@students.lincoln.ac.uk>
2020-03-06 20:36:42 +0000

    Add project files.

---
+++ OOPAssignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace OOPAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = File.ReadAllLines("text1.txt");
+            var string2 = File.ReadAllLines("text2.txt");
+            Console.WriteLine(string1.SequenceEqual(string2) ? "These are the same!" : "These aren't the same");
+        }
+    }
+}
