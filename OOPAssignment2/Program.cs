7e5f12b39ffd3a57fba86144275590d40660465e
Brandon-Lee Dodds <19696909@students.lincoln.ac.uk>
2020-03-09 14:43:25 +0000

    Works.

--- OOPAssignment2/Program.cs
+++ OOPAssignment2/Program.cs
@@ -10,15 +10,27 @@ namespace OOPAssignment2
         private static readonly HashAlgorithm hashingAlgorithm = SHA512.Create();
         private static byte[] GetShaHash(string filename)
         {
-            using FileStream stream = File.OpenRead(filename);
-            return hashingAlgorithm.ComputeHash(stream);
+            byte[] hash = default;
+            try
+            {
+                using FileStream stream = File.OpenRead(filename);
+                hash = hashingAlgorithm.ComputeHash(stream);
+            } 
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found, sending default value.");
+            }
+            return hash;
         }
 
         static void Main()
         {
-            var hash1 = GetShaHash("text1.txt");
+            var hash1 = GetShaHash("tex1.txt");
             var hash2 = GetShaHash("text2.txt");
-            Console.WriteLine(hash1.SequenceEqual(hash2) ? "These are the same!" : "These aren't the same");
+            if(hash1 != null && hash2 != null)
+                Console.WriteLine(hash1.SequenceEqual(hash2) ? "These are the same!" : "These aren't the same");
+            else
+                Console.WriteLine("Hash could not be computed.");
         }
     }
 }
