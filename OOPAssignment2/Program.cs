59a3ad2122c970bcf48017427673549efc6227fb
Brandon-Lee Dodds <19696909@students.lincoln.ac.uk>
2020-03-10 10:03:04 +0000

    Made my code look pretty.

--- OOPAssignment2/Program.cs
+++ OOPAssignment2/Program.cs
@@ -18,19 +18,57 @@ namespace OOPAssignment2
             } 
             catch (FileNotFoundException)
             {
-                Console.WriteLine("The file was not found, sending default value.");
+                Console.WriteLine($"The file {filename} was not found, sending default value.");
             }
             return hash;
         }
 
+        public static void Diff(string path1, string path2)
+        {
+            var hash1 = GetShaHash(path1);
+            var hash2 = GetShaHash(path2);
+            if (hash1 != null && hash2 != null)
+            {
+                if (hash1.SequenceEqual(hash2))
+                {
+                    Console.Write(">: [Output] ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{path1} and {path2} are not different. \n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(">: [Output] ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{path1} and {path2} are different. \n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hash could not be computed due to null values.");
+            }
+        }
+
         static void Main()
         {
-            var hash1 = GetShaHash("tex1.txt");
-            var hash2 = GetShaHash("text2.txt");
-            if(hash1 != null && hash2 != null)
-                Console.WriteLine(hash1.SequenceEqual(hash2) ? "These are the same!" : "These aren't the same");
-            else
-                Console.WriteLine("Hash could not be computed.");
+            while (true)
+            {
+                Console.Write(">: [INPUT] ");
+                var userCommandAndArgs = Console.ReadLine();
+                var userSplit = userCommandAndArgs.Split(" ");
+                switch (userSplit[0])
+                {
+                    case "diff":
+                        Diff(userSplit[1], userSplit[2]);
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid command.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
         }
     }
 }
